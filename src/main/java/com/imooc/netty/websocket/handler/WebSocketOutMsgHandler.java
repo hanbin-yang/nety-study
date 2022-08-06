54b30dd1ccb8d4054a5cc81fe666600fc2547e70
yanghanbin <123456@qq.com>
2022-08-06 22:08:02 +0800

    TextWebSocketFrame

--- src/main/java/com/imooc/netty/websocket/handler/WebSocketOutMsgHandler.java
+++ src/main/java/com/imooc/netty/websocket/handler/WebSocketOutMsgHandler.java
@@ -4,6 +4,7 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 
 /**
  * 出站消息的处理器
@@ -14,6 +15,9 @@ public class WebSocketOutMsgHandler extends ChannelOutboundHandlerAdapter {
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
         System.out.println("发送数据， msg=" + msg);
+        if (msg instanceof String) {
+            super.write(ctx, new TextWebSocketFrame("return message: " + msg), promise);
+        }
         super.write(ctx, msg, promise);
     }
 }
